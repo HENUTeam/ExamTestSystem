3e1093b63aa14060a265916f12582087d8b1ff95
高逸博 <1468165899@qq.com>
2017-11-17 17:08:24 +0800

    bug 修改

--- src/main/java/com/henu/examtestsystem/ExamtestsystemApplication.java
+++ src/main/java/com/henu/examtestsystem/ExamtestsystemApplication.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 
 @Controller
 @SpringBootApplication
@@ -31,23 +32,38 @@ public class ExamtestsystemApplication {
     }
 
     @RequestMapping(value = "/login")
-    public String login(ModelMap modelMap, String name, String password) {
+    public String login(ModelMap modelMap, String name, String password,
+                        HttpSession session) {
         logger.info("---------name:{},password:{}", name, password);
         Teacher teacher = teacherService.findByTeaid(name);
         if (teacher != null) {
             logger.info("---------------name:{}", teacher.getName());
             if (password.equals(teacher.getPassword())) {
                 logger.info("-----------密码正确");
-                return "teacher/index";
+                session.setAttribute("user", teacher);
+                return "redirect:/teacher";
             }
         }
         User user = userService.findByStuid(name);
         if (user != null) {
             logger.info("---------------name:{}", user.getName());
-            return "student/index";
+            if (password.equals(user.getPassword())) {
+                session.setAttribute("user", user);
+                return "redirect:/student";
+            }
         }
         modelMap.addAttribute("iserro", true);
         return "index";
+    }
+
+    @RequestMapping(value = "/student")
+    public String goStudent() {
+        return "student/index";
+    }
+
+    @RequestMapping(value = "/teacher")
+    public String goTeacher() {
+        return "teacher/index";
     }
 
     public static void main(String[] args) {
