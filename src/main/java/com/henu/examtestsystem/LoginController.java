2545ae72f1cfb5cacb3d5431699afdd166aa2953
高逸博 <1468165899@qq.com>
2017-11-30 16:31:36 +0800

    bug修改

--- src/main/java/com/henu/examtestsystem/LoginController.java
+++ src/main/java/com/henu/examtestsystem/LoginController.java
@@ -1,10 +1,17 @@
 package com.henu.examtestsystem;
 
+import com.henu.examtestsystem.student.bean.User;
+import com.henu.examtestsystem.student.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
+
+    @Autowired
+    UserRepository userRepository;
 
     @RequestMapping("/403")
     public String error() {
@@ -14,6 +21,16 @@ public class LoginController {
     @RequestMapping(value = {"/", "/login"})
     public String home() {
         return "index";
+    }
+
+    @RequestMapping(value = {"/togo"})
+    public String go(String name, String password, ModelMap modelMap) {
+        User user = userRepository.findByName(name);
+        if (user == null) {
+            modelMap.addAttribute("info_erro", true);
+            return "index";
+        }
+        return "redirect:/";
     }
 
 
