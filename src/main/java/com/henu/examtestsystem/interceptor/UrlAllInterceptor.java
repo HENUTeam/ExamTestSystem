35f7f0ee59cc52a94c52ec1d8af4aa41218b79cb
henugyb <1468165899@qq.com>
2017-12-08 17:02:43 +0800

    修改
    Signed-off-by: henugyb <1468165899@qq.com>

--- src/main/java/com/henu/examtestsystem/interceptor/UrlAllInterceptor.java
+++ src/main/java/com/henu/examtestsystem/interceptor/UrlAllInterceptor.java
@@ -2,6 +2,7 @@ package com.henu.examtestsystem.interceptor;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
